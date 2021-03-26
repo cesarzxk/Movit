49d7a368d0056a3c4e8cb4f5ee39019d8e7659d6
César Vargas Dos Santos <43748428+cesarzxk@users.noreply.github.com>
2021-03-26 16:08:11 -0300

    Update main.tsx

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Head from'next/head';
 import {GetServerSideProps} from 'next';
 import {ChallengeProvider} from '../contexts/ChallengeContext';
-
 
 import { ExperienceBar } from "../components/ExperienceBar";
 import {Changecolor} from "../components/ChangeColor";
@@ -12,14 +11,28 @@ import CompletedChallenges from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { GlobalContext } from '../contexts/GlobalContext';
 
 type PropsDate = {
       level:number;
       currentExperience:number; 
       challengesCompleted:number;
+      darkMod: string;
 }
 
 export default function Main(props:PropsDate) {
+
+  const {changelight} = useContext(GlobalContext);
+
+  useEffect(()=>{
+    if (props.darkMod == 'true'){
+        changelight(true);
+    }else{
+        changelight(false);
+    }
+},[])
+    
+
 
   return (
     <ChallengeProvider 
@@ -53,14 +66,15 @@ export default function Main(props:PropsDate) {
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
-    const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+    const {level, currentExperience, challengesCompleted, darkMod} = ctx.req.cookies;
     console.log(currentExperience);
 
  return{
     props :{
       level: (Number(level) | 1), 
       currentExperience: (Number(currentExperience) || 0), 
-      challengesCompleted:(Number(challengesCompleted) || 0)
+      challengesCompleted:(Number(challengesCompleted) || 0),
+      darkMod:darkMod
     }
   };
 }
